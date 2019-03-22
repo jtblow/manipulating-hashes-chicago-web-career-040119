71f171ba9006fc6e265cb87fd647e29a88340d83
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-22 16:20:33 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,8 @@ def first_challenge
     }
   }
 
-  contacts["Freddy_Mercury"].delete_if do 
+  contacts["Freddy_Mercury"].delete_if do |key, value|
+    :favorite_icecream_flavors 
     
   end
 
